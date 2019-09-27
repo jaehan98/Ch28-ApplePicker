ade8537a5e787c149c103e7dd7ebeb078d442cd5
jaehan98 <43914363+jaehan98@users.noreply.github.com>
2019-09-27 17:33:34 -0400

    ApplePicker

---
+++ Assets/Scripts/HighScore.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour
+{
+    static public int score = 1000;
+
+    void Awake()
+    {                                                         
+        // If the PlayerPrefs HighScore already exists, read it
+        if (PlayerPrefs.HasKey("HighScore"))
+        {                               // b
+            score = PlayerPrefs.GetInt("HighScore");
+        }
+        // Assign the high score to HighScore
+        PlayerPrefs.SetInt("HighScore", score);                              
+    }
+
+    void Update()
+    {
+        Text gt = this.GetComponent<Text>();
+        gt.text = "High Score: " + score;
+        // Update the PlayerPrefs HighScore if necessary
+        
+      
+    }
+
+// Start is called before the first frame update
+void Start()
+    {
+        
+    }
+
+    
+}
---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public float delay = 18000;
+    public string NewLevel = "Level 2";
+    void Start()
+    {
+        StartCoroutine(LoadLevelAfterDelay(delay));
+    }
+
+    IEnumerator LoadLevelAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(NewLevel);
+    }
+}
